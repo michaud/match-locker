c72da47efa53db44a81df241e82e1e7de4ebddb8
Michaud <michaud@venant.nl>
2026-01-16 14:40:18 +0100

    small utility

---
+++ scripts/svg_file_generation.py
@@ -0,0 +1,107 @@
+docs = [
+["d://projects//match-locker//src//games//minecraftEggs//allay_spawn_egg.svg","allay"],
+["d://projects//match-locker//src//games//minecraftEggs//armadillo_spawn_egg.svg","armadillo"],
+["d://projects//match-locker//src//games//minecraftEggs//axolotl_spawn_egg.svg","axolotl"],
+["d://projects//match-locker//src//games//minecraftEggs//bat_spawn_egg.svg","bat"],
+["d://projects//match-locker//src//games//minecraftEggs//bee_spawn_egg.svg","bee"],
+["d://projects//match-locker//src//games//minecraftEggs//blaze_spawn_egg.svg","blaze"],
+["d://projects//match-locker//src//games//minecraftEggs//bogged_spawn_egg.svg","bogged"],
+["d://projects//match-locker//src//games//minecraftEggs//breeze_spawn_egg.svg","breeze"],
+["d://projects//match-locker//src//games//minecraftEggs//camel_husk_spawn_egg.svg","camel husk"],
+["d://projects//match-locker//src//games//minecraftEggs//camel_spawn_egg.svg","camel"],
+["d://projects//match-locker//src//games//minecraftEggs//cat_spawn_egg.svg","cat"],
+["d://projects//match-locker//src//games//minecraftEggs//cave_spider_spawn_egg.svg","cave spider"],
+["d://projects//match-locker//src//games//minecraftEggs//chicken_spawn_egg.svg","chicken"],
+["d://projects//match-locker//src//games//minecraftEggs//cod_spawn_egg.svg","cod"],
+["d://projects//match-locker//src//games//minecraftEggs//copper_golem_spawn_egg.svg","copper golem"],
+["d://projects//match-locker//src//games//minecraftEggs//cow_spawn_egg.svg","cow"],
+["d://projects//match-locker//src//games//minecraftEggs//creaking_spawn_egg.svg","creaking"],
+["d://projects//match-locker//src//games//minecraftEggs//creeper_spawn_egg.svg","creeper"],
+["d://projects//match-locker//src//games//minecraftEggs//dolphin_spawn_egg.svg","dolphin"],
+["d://projects//match-locker//src//games//minecraftEggs//donkey_spawn_egg.svg","donkey"],
+["d://projects//match-locker//src//games//minecraftEggs//drowned_spawn_egg.svg","drowned"],
+["d://projects//match-locker//src//games//minecraftEggs//elder_guardian_spawn_egg.svg","elder guardian"],
+["d://projects//match-locker//src//games//minecraftEggs//ender_dragon_spawn_egg.svg","ender dragon"],
+["d://projects//match-locker//src//games//minecraftEggs//enderman_spawn_egg.svg","enderman"],
+["d://projects//match-locker//src//games//minecraftEggs//endermite_spawn_egg.svg","endermite"],
+["d://projects//match-locker//src//games//minecraftEggs//evoker_spawn_egg.svg","evoker"],
+["d://projects//match-locker//src//games//minecraftEggs//fox_spawn_egg.svg","fox"],
+["d://projects//match-locker//src//games//minecraftEggs//frog_spawn_egg.svg","frog"],
+["d://projects//match-locker//src//games//minecraftEggs//ghast_spawn_egg.svg","ghast"],
+["d://projects//match-locker//src//games//minecraftEggs//glow_squid_spawn_egg.svg","glow squid"],
+["d://projects//match-locker//src//games//minecraftEggs//goat_spawn_egg.svg","goat"],
+["d://projects//match-locker//src//games//minecraftEggs//guardian_spawn_egg.svg","guardian"],
+["d://projects//match-locker//src//games//minecraftEggs//happy_ghast_spawn_egg.svg","happy ghast"],
+["d://projects//match-locker//src//games//minecraftEggs//hoglin_spawn_egg.svg","hoglin"],
+["d://projects//match-locker//src//games//minecraftEggs//horse_spawn_egg.svg","horse"],
+["d://projects//match-locker//src//games//minecraftEggs//husk_spawn_egg.svg","husk"],
+["d://projects//match-locker//src//games//minecraftEggs//iron_golem_spawn_egg.svg","iron golem"],
+["d://projects//match-locker//src//games//minecraftEggs//llama_spawn_egg.svg","llama"],
+["d://projects//match-locker//src//games//minecraftEggs//magma_cube_spawn_egg.svg","magma cube"],
+["d://projects//match-locker//src//games//minecraftEggs//mooshroom_spawn_egg.svg","mooshroom"],
+["d://projects//match-locker//src//games//minecraftEggs//mule_spawn_egg.svg","mule"],
+["d://projects//match-locker//src//games//minecraftEggs//nautilus_spawn_egg.svg","nautilus"],
+["d://projects//match-locker//src//games//minecraftEggs//ocelot_spawn_egg.svg","ocelot"],
+["d://projects//match-locker//src//games//minecraftEggs//panda_spawn_egg.svg","panda"],
+["d://projects//match-locker//src//games//minecraftEggs//parched_spawn_egg.svg","parched"],
+["d://projects//match-locker//src//games//minecraftEggs//parrot_spawn_egg.svg","parrot"],
+["d://projects//match-locker//src//games//minecraftEggs//phantom_spawn_egg.svg","phantom"],
+["d://projects//match-locker//src//games//minecraftEggs//pig_spawn_egg.svg","pig"],
+["d://projects//match-locker//src//games//minecraftEggs//piglin_brute_spawn_egg.svg","piglin brute"],
+["d://projects//match-locker//src//games//minecraftEggs//piglin_spawn_egg.svg","piglin"],
+["d://projects//match-locker//src//games//minecraftEggs//pillager_spawn_egg.svg","pillager"],
+["d://projects//match-locker//src//games//minecraftEggs//polar_bear_spawn_egg.svg","polar bear"],
+["d://projects//match-locker//src//games//minecraftEggs//pufferfish_spawn_egg.svg","pufferfish"],
+["d://projects//match-locker//src//games//minecraftEggs//rabbit_spawn_egg.svg","rabbit"],
+["d://projects//match-locker//src//games//minecraftEggs//ravager_spawn_egg.svg","ravager"],
+["d://projects//match-locker//src//games//minecraftEggs//salmon_spawn_egg.svg","salmon"],
+["d://projects//match-locker//src//games//minecraftEggs//sheep_spawn_egg.svg","sheep"],
+["d://projects//match-locker//src//games//minecraftEggs//shulker_spawn_egg.svg","shulker"],
+["d://projects//match-locker//src//games//minecraftEggs//silverfish_spawn_egg.svg","silverfish"],
+["d://projects//match-locker//src//games//minecraftEggs//skeleton_horse_spawn_egg.svg","skeleton horse"],
+["d://projects//match-locker//src//games//minecraftEggs//skeleton_spawn_egg.svg","skeleton"],
+["d://projects//match-locker//src//games//minecraftEggs//slime_spawn_egg.svg","slime"],
+["d://projects//match-locker//src//games//minecraftEggs//sniffer_spawn_egg.svg","sniffer"],
+["d://projects//match-locker//src//games//minecraftEggs//snow_golem_spawn_egg.svg","snow golem"],
+["d://projects//match-locker//src//games//minecraftEggs//spider_spawn_egg.svg","spider"],
+["d://projects//match-locker//src//games//minecraftEggs//squid_spawn_egg.svg","squid"],
+["d://projects//match-locker//src//games//minecraftEggs//stray_spawn_egg.svg","stray"],
+["d://projects//match-locker//src//games//minecraftEggs//strider_spawn_egg.svg","strider"],
+["d://projects//match-locker//src//games//minecraftEggs//tadpole_spawn_egg.svg","tadpole"],
+["d://projects//match-locker//src//games//minecraftEggs//trader_llama_spawn_egg.svg","trader llama"],
+["d://projects//match-locker//src//games//minecraftEggs//tropical_fish_spawn_egg.svg","tropical fish"],
+["d://projects//match-locker//src//games//minecraftEggs//turtle_spawn_egg.svg","turtle"],
+["d://projects//match-locker//src//games//minecraftEggs//vex_spawn_egg.svg","vex"],
+["d://projects//match-locker//src//games//minecraftEggs//villager_spawn_egg.svg","villager"],
+["d://projects//match-locker//src//games//minecraftEggs//vindicator_spawn_egg.svg","vindicator"],
+["d://projects//match-locker//src//games//minecraftEggs//wandering_trader_spawn_egg.svg","wandering trader"],
+["d://projects//match-locker//src//games//minecraftEggs//warden_spawn_egg.svg","warden"],
+["d://projects//match-locker//src//games//minecraftEggs//witch_spawn_egg.svg","witch"],
+["d://projects//match-locker//src//games//minecraftEggs//wither_skeleton_spawn_egg.svg","wither skeleton"],
+["d://projects//match-locker//src//games//minecraftEggs//wither_spawn_egg.svg","wither"],
+["d://projects//match-locker//src//games//minecraftEggs//wolf_spawn_egg.svg","wolf"],
+["d://projects//match-locker//src//games//minecraftEggs//zoglin_spawn_egg.svg","zoglin"],
+["d://projects//match-locker//src//games//minecraftEggs//zombie_horse_spawn_egg.svg","zombie horse"],
+["d://projects//match-locker//src//games//minecraftEggs//zombie_nautilus_spawn_egg.svg","zombie nautilus"],
+["d://projects//match-locker//src//games//minecraftEggs//zombie_spawn_egg.svg","zombie"],
+["d://projects//match-locker//src//games//minecraftEggs//zombie_villager_spawn_egg.svg","zombie villager"],
+["d://projects//match-locker//src//games//minecraftEggs//zombified_piglin_spawn_egg.svg","zombified piglin"]]
+
+svg_doc = '''<?xml version="1.0" encoding="UTF-8"?>
+<svg width="960" height="680" version="1.1" viewBox="0 0 960 680" xmlns="http://www.w3.org/2000/svg">
+    <defs>
+        <filter id="b" x="-.46967" y="-1.6964" width="1.9393" height="4.3927" style="color-interpolation-filters:sRGB">
+            <feFlood flood-color="rgb(0,0,0)" in="SourceGraphic" result="flood" />
+            <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="100" />
+            <feOffset dx="0" dy="0" in="blur" result="offset" />
+            <feComposite in="flood" in2="offset" operator="in" result="comp1" />
+            <feComposite in="SourceGraphic" in2="comp1" result="comp2" />
+        </filter>
+    </defs>
+    <text x="475.27594" y="600" style="filter:url(#b);" xml:space="preserve"><tspan style="fill:#ffffff;font-family:Sans;font-size:127px;font-weight:bold;text-align:center;text-anchor:middle;">{0}</tspan></text>
+</svg>'''
+
+for doc in docs:
+    with open(doc[0], 'x', encoding='utf-8') as f:
+        f.write(svg_doc.format(doc[1]))
+        f.close()
